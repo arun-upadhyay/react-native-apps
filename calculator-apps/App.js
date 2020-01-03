557d2da0d14412a8da13655b2558dfafe719f882
Arun Upadhyay <arunnnet@gmail.com>
2020-01-02 21:22:14 -0500

    Layout for calculator app

--- calculator-apps/App.js
+++ calculator-apps/App.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import CalculatorBar from './CalculatorBar';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <CalculatorBar />
-      <Text> Example Test </Text>
-    </View>
-  );
+    return (
+        <CalculatorBar></CalculatorBar>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- calculator-apps/CalculatorBar.js
+++ calculator-apps/CalculatorBar.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
-import { render } from 'react-dom';
-export default class CalculatorBar extends Component
-{
+import { View, TextInput, Text } from 'react-native';
+import Style from './Style';
+import InputButton from "./InputButton";
 
-    render(){
+const inputButtons = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+']
+];
+
+export default class CalculatorBar extends Component {
+    render() {
         return (
-            <View>
-              
-              
-
-              <TextInput placeholder="Enter your text here.." ></TextInput>
-
-
+            <View style={Style.rootContainer}>
+                <View style={Style.displayContainer}>
+                    <TextInput> Enter your Text</TextInput>
+                </View>
+                <View style={Style.inputContainer}>
+                    {this._renderInputButtons()}
+                </View>
             </View>
         );
     };
+    _renderInputButtons() {
+        let views = [];
+        for (let r = 0; r < inputButtons.length; r ++) {
+            let row = inputButtons[r];
+            let inputRow = [];
+            for (let i = 0; i < row.length; i ++) {
+                let input = row[i];
+
+                inputRow.push(
+                    <InputButton value={input} key={r + "-" + i} />
+                );
+            }
+            views.push(<View style={Style.inputRow} key={"row-" + r}>{inputRow}</View>)
+        }
+
+        return views;
+    }
+
 };
